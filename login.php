5b0096f73aa33e5cc57ffbe4d73e5a1515ced289
sampweise <sampweise@gmail.com>
2022-04-21 13:02:52 -0500

    added cgi script and htacess to write php inline html

--- login.php
+++ login.php
@@ -1,12 +1,10 @@
 <?php
+session_start();
 $mysqli = new mysqli("mysql.eecs.ku.edu", "s133w404", "ahj3Hugh", "s133w404");
 
 if ($mysqli->connect_errno) {
   printf("Connect failed: %s\n", $mysqli->connect_error); exit();
   echo "<h1>Fail</h1>";
-}
-else {
-  //echo "<h1>Success</h1>";
 }
 
 $isSuccess=0;
@@ -22,6 +20,7 @@ if ($result->num_rows > 0) {
       $dbPassword = $row["UserPassword"];
       if($dbPassword == $userPassword)
       {
+          $_SESSION['user']=$row["UserID"];
           $isSuccess=1;
       }
   }
@@ -35,7 +34,6 @@ if ($isSuccess == 0) {
     header("Location: index.html");
 } else 
 {
-    echo "<h1>Successss</h1>";
     header("Location: shop.html");
 }
 
